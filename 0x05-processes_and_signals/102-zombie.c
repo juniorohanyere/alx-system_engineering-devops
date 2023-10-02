eff536905d4aeef641d440dfa096aa0b18aa966c
Twin J <junohanyere@gmail.com>
2023-10-02 14:27:54 +0100

    create five zombie processes

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/**
+ * infinite_while - infinite loop
+ *
+ * Return: always return 0
+*/
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - entry point
+ *
+ * Description: creates 5 zombie processes
+ *
+ * Return: always return 0
+*/
+
+int main(void)
+{
+	int i;
+	pid_t pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+		}
+
+		else
+			exit(EXIT_SUCCESS);
+	}
+
+	infinite_while();
+	return (0);
+}
