2d6ab177ce24f7a3b15fbfa4637db6ae7c9e0bda
Twin J <junohanyere@gmail.com>
2023-11-24 13:55:59 +0100

    puppet

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# install the puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# use exec to kill a particular process
+exec {'kill-killmenow':
+  command => 'pkill killmenow',
+  path    => '/usr/bin';
+}
